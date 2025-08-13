0bce9b3baba49501de88166d81e1463921fd145f
[dyad] <sxdiaz7@gmail.com>
2025-08-13 14:13:12 -0700

    [dyad] Set up Supabase client and Toaster - wrote 5 file(s), added @supabase/supabase-js package(s)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { LanguageProvider } from "@/contexts/LanguageContext";
+import { Toaster } from "@/components/ui/sonner"; // Import Toaster
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,7 +29,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <LanguageProvider>{children}</LanguageProvider>
+        <LanguageProvider>
+          {children}
+          <Toaster /> {/* Add Toaster here */}
+        </LanguageProvider>
       </body>
     </html>
   );
